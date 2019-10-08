1d4d8812ce8b35df0e15feceed366e117d73e94c
girinssh <tngus5851@naver.com>
2019-10-08 16:59:08 +0900

    Class Structure

--- ActiveEXECsharp/Program.cs
+++ ActiveEXECsharp/Program.cs
@@ -7,65 +7,73 @@ using System.Net;
 using System.Collections;
 
 namespace CSharpLanguage {
-
-	class CompanyTreeNode {
+	class CompanyDepartmentTreeNode {
 		public int level;
-		public Dictionary<string, CompanyTreeNode> data;
+		public List<string> data;
 
-		public CompanyTreeNode(int level = 0) {
+		public CompanyDepartmentTreeNode(int level = 0) {
 			this.level = level;
-			data = new Dictionary<string, CompanyTreeNode>();
+			data = new List<string>();
 		}
 	}
 
-	class CompanyTree {
-		public CompanyTreeNode root;
+	class CompanyDepartmentTree {
+		public CompanyDepartmentTreeNode root;
 
-		public CompanyTree() {
-			this.root = new CompanyTreeNode(0);
+		public CompanyDepartmentTree() {
+			this.root = new CompanyDepartmentTreeNode(0);
 		}
 		public int AddNode(string key, string path = "/") {
 
-			string[] nodePath = path.Split('/');
-			CompanyTreeNode nowNode = root;
-			Console.WriteLine("{0}", nodePath.Length);
-			foreach (string selection in nodePath) {
-				if (selection == "")
-					break;
-				nowNode = nowNode.data[selection];
-			}
-			CompanyTreeNode department = new CompanyTreeNode(nowNode.level + 1);
-
-			nowNode.data.Add(key, department);
-
 			return 1;
 		}
+
+		public int DeleteNode(string key, string path = "/") {
+			return 1;
+		}
+		public string FindElement(string element) {
+			string path = "";
+			return path;
+		}
+
+		public void PrintList(string path = "/") {
+
+		}
+		public void PrintAllElement(string path = "/") {
+
+		}
 	}
-	//  class User {
-	//      private string user_id;
-	//      private string user_password;
 
+	class User {
+		private CompanyDepartmentTree company;
+		private string nowPath;
 
-	//      public void getUser() {
-	//      }
-	//      public void setDepartments() {
-	//}
+		public User() {
+			this.nowPath = "/";
+			this.company = new CompanyDepartmentTree();
+		}
 
-	//  }
+		public void AddDepartments(string path = "/") {
+		}
+		public void InstallPrograms() {
+
+		}
+		public void LoadProgramList() {
+
+		}
+		public void MoveDepartment() {
+
+		}
+		public void AddPrograms() {
+
+		}
+	}
 
 	class Program {
 		private bool nowLogin = false;
 
 		static void Main(string[] args) {
-			CompanyTree company = new CompanyTree();
-			company.AddNode("Program");
-			company.AddNode("AI", path: "Program/");
-			company.AddNode("asdf", path: "Program/AI/");
-
-			Console.WriteLine(company.root.data.ContainsKey("Program"));
-			Console.WriteLine(company.root.data["Program"].data.ContainsKey("AI"));
-			Console.WriteLine(company.root.data["Program"].data["AI"].data.ContainsKey("asdf"));
-
+			User user = new User();
 		}
 	}
 }
